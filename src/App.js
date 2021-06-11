8b230da092c4c10a162e80b0c20fc53edaf1d46c
Michael Salgado <michaelsalgadop11@gmail.com>
2021-06-11 13:25:09 +0200

    Se pintan las targetas de los senyores vacias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Cabecera } from "./componentes/Cabecera";
+import { Senyor } from "./componentes/Senyor";
+import { senyoresAPI as senyores } from "./senyores.js";
 function App() {
-  return <p>Hola</p>;
+  return (
+    <div className="contenedor-general container-xl">
+      <Cabecera />
+      <main className="principal mt-2 row">
+        {senyores.map((senyor) => (
+          <Senyor key={senyor.id} />
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export default App;
